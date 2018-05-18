fb6e932228301dd8e6ef9a34d23956ce9d412fa9
junbang qin <junbangqin@junbangdeMacBook-Pro.local>
2018-05-18 23:07:08 +0800

    首页导航

---
+++ public/bootstrap_common.js
@@ -0,0 +1,8 @@
+/**
+ * Created by junbangqin on 2018/5/18.
+ */
+var path = "/AdminLTE-2.4.3/";
+document.write("<link rel='stylesheet' href=" + path + 'bower_components/bootstrap/dist/css/bootstrap.min.css' + " />");
+document.write("<link rel='stylesheet' href='./css/index.css'/>");
+document.write("<script src=" + path + 'bower_components/jquery/dist/jquery.min.js' + "></script>");
+document.write("<script src=" + path + 'bower_components/bootstrap/dist/js/bootstrap.min.js' + "></script>");
